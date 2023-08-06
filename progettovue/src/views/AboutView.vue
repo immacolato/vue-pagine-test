7d99fedcfb069b30538dd522af1784b9380f3958
Luca Arenella <luca.arenella@edu.unito.it>
2023-08-06 19:08:07 +0200

    test commit

--- progettovue/src/views/AboutView.vue
+++ progettovue/src/views/AboutView.vue
@@ -15,6 +15,17 @@
     <h1>TEST per SIMONE !!! blablabla</h1>
     <h1>TEST per SIMONE</h1>
     <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
+    <h1>TEST per SIMONE</h1>
+    <h1>TEST per SIMONE !!! blablabla</h1>
 
   </div>
 </template>
